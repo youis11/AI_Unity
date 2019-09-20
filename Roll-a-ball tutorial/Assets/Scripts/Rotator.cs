3120c9a57d2b630540b84e98247a5a6843f3b739
Lluís Moreu <36155329+youis11@users.noreply.github.com>
2019-09-20 21:57:58 +0200

    added roll a ball

---
+++ Roll-a-ball tutorial/Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rotation = new Vector3(15, 30, 45);
+        transform.Rotate(rotation * Time.deltaTime);
+    }
+}
---
+++ Roll-a-ball tutorial/README.md
@@ -0,0 +1,6 @@
+# Roll a Ball tutorial
+
+This is the result of an official Unity tutorial.
+
+Here's the website: https://unity3d.com/es/learn/tutorials/s/roll-ball-tutorial
+
