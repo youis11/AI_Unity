5d07604e6eff8e18f13917b1ef8d0a9e85e16989
youis11 <lluis.moreu@gmail.com>
2019-09-24 18:24:25 +0200

    pick ups repel and attract to player

--- Roll-a-ball tutorial/Assets/Scripts/Rotator.cs
+++ Roll-a-ball tutorial/Assets/Scripts/Rotator.cs
@@ -4,10 +4,46 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+    public GameObject player;
+    
+    private bool come = false;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
     // Update is called once per frame
     void Update()
     {
         Vector3 rotation = new Vector3(15, 30, 45);
         transform.Rotate(rotation * Time.deltaTime);
+
+        int velocity = 0;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (come == true)
+            {
+                come = false;
+            }
+
+            else
+            {
+                come = true;
+            }
+        }
+       
+
+        if (come == false)
+        {
+            velocity = 5;
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, velocity * Time.deltaTime);
+        }
+        else
+        {
+            velocity = -5;
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, velocity * Time.deltaTime);
+        }
     }
 }
