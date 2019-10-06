45ea1b82688ae384ea5db9f1564596ba56c54211
Lluís Moreu <36155329+youis11@users.noreply.github.com>
2019-10-06 17:58:41 +0200

    tank 3 upload

---
+++ TANK 3/Assets/Steering/SteeringObstacleAvoidance.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteeringObstacleAvoidance : MonoBehaviour {
+
+	public LayerMask mask;
+	public float avoid_distance = 5.0f;
+
+
+	Move move;
+	SteeringSeek seek;
+
+	// Use this for initialization
+	void Start () {
+		move = GetComponent<Move>(); 
+		seek = GetComponent<SteeringSeek>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// TODO 2: Agents must avoid any collider in their way
+		// 1- Create your own (serializable) class for rays and make a public array with it
+		// 2- Calculate a quaternion with rotation based on movement vector
+		// 3- Cast all rays. If one hit, get away from that surface using the hitpoint and normal info
+		// 4- Make sure there is debug draw for all rays (below in OnDrawGizmosSelected)
+	}
+
+	void OnDrawGizmosSelected() 
+	{
+		if(move && this.isActiveAndEnabled)
+		{
+			Gizmos.color = Color.red;
+			float angle = Mathf.Atan2(move.movement_vel.x, move.movement_vel.z);
+			Quaternion q = Quaternion.AngleAxis(Mathf.Rad2Deg * angle, Vector3.up);
+
+			// TODO 2: Debug draw thoise rays (Look at Gizmos.DrawLine)
+
+		}
+	}
+}
---
+++ TANK 3/Assets/Steering/SteeringSeparation.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteeringSeparation : MonoBehaviour {
+
+	public LayerMask mask;
+	public float search_radius = 5.0f;
+	public AnimationCurve strength;
+
+	Move move;
+
+	// Use this for initialization
+	void Start () {
+		move = GetComponent<Move>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// TODO 1: Agents much separate from each other:
+		// 1- Find other agents in the vicinity (use a layer for all agents)
+		// 2- For each of them calculate a escape vector using the AnimationCurve
+		// 3- Sum up all vectors and trim down to maximum acceleration
+	}
+
+	void OnDrawGizmosSelected() 
+	{
+		// Display the explosion radius when selected
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere(transform.position, search_radius);
+	}
+}
