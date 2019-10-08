186c3baaedfa9f23adfa54ca3d9c372d5f213e66
Lluís Moreu <36155329+youis11@users.noreply.github.com>
2019-10-08 09:33:04 +0200

    tank 4

---
+++ TANK 4/Assets/FollowCurve.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BansheeGz.BGSpline.Components;
+using BansheeGz.BGSpline.Curve;
+
+public class FollowCurve : MonoBehaviour
+{
+    private float ratio;
+    public BGCcMath path;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //increase distance
+
+        if(ratio < 1.0f)
+            ratio += 0.1f * Time.deltaTime;
+
+        if (ratio >= 1.0f)
+            ratio = 0.0f;
+
+        //calculate position and tangent
+        transform.position = path.CalcPositionByDistanceRatio(ratio);
+        //this is a version for 3D. For 2D, comment this line and uncomment the next one
+        //transform.rotation = Quaternion.LookRotation(tangent);
+        //ObjectToMove.rotation = Quaternion.AngleAxis(Mathf.Atan2(tangent.y, tangent.x) * Mathf.Rad2Deg, Vector3.forward);
+    }
+}
---
+++ TANK 4/Assets/Steering/SteeringFollowPath.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using BansheeGz.BGSpline.Components;
+using BansheeGz.BGSpline.Curve;
+
+public class SteeringFollowPath : MonoBehaviour {
+
+	Move move;
+	SteeringSeek seek;
+
+    public float ratio_increment = 0.1f;
+    public float min_distance = 1.0f;
+    float current_ratio = 0.0f;
+
+    public BGCcMath path;
+    Vector3 closestPoint;
+
+    // Use this for initialization
+    void Start () {
+		move = GetComponent<Move>();
+		seek = GetComponent<SteeringSeek>();
+
+        // TODO 1: Calculate the closest point from the tank to the curve
+        float distance;
+        closestPoint = path.CalcPositionByClosestPoint(transform.position, out distance);
+
+        current_ratio = distance / path.GetDistance();
+    }
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        // TODO 2: Check if the tank is close enough to the desired point
+        // If so, create a new point further ahead in the path
+        Vector3 target = closestPoint - transform.position;
+
+        if (target.magnitude < min_distance)
+        {
+            current_ratio += ratio_increment;
+
+            if (current_ratio >= 1.0f)
+                current_ratio = 0.0f;
+
+            closestPoint = path.CalcPositionByDistanceRatio(current_ratio);
+        }
+
+        seek.Steer(closestPoint);
+    }
+
+	void OnDrawGizmosSelected() 
+	{
+
+		if(isActiveAndEnabled)
+		{
+			// Display the explosion radius when selected
+			Gizmos.color = Color.green;
+			// Useful if you draw a sphere were on the closest point to the path
+		}
+
+	}
+}
