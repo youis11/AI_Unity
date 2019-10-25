4a01e617dff350448859a71a001f1f0e7920738d
Lluís Moreu <36155329+youis11@users.noreply.github.com>
2019-10-25 11:57:25 +0200

    animation unity wip

---
+++ Animation/Assets/MoveDude.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveDude : MonoBehaviour
+{
+    Animator Animator;
+    NavMeshAgent Agent;
+    Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+        Agent = GetComponent<NavMeshAgent>();
+        target = FindObjectOfType<MoveToMouseClick>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Agent.SetDestination(target.position);
+        if((transform.position - target.position).magnitude < 2)
+        {
+            Animator.SetBool("moving", false);
+        }
+        else
+        {
+            Animator.SetBool("moving", true);
+        }
+    }
+}
