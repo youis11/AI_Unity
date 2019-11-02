b1f34452e8cd0bdbd4caadd04c8f1906780b4a93
youis11 <lluis.moreu@gmail.com>
2019-11-02 13:55:01 +0100

    reworking arrive and wander

--- TANK 4/Assets/Steering/SteeringArrive.cs
+++ TANK 4/Assets/Steering/SteeringArrive.cs
@@ -1,65 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SteeringArrive : Steering {
+public class SteeringArrive : Steering
+{
 
     public float min_distance = 0.1f;
     public float slow_distance = 5.0f;
-    public float time_to_accel = 0.1f;
+    public float time_to_accel = 1.0f;
 
     Move move;
 
-	// Use this for initialization
-	void Start () { 
-		move = GetComponent<Move>();
-	}
+    // Use this for initialization
+    void Start()
+    {
+        move = GetComponent<Move>();
+    }
 
-	// Update is called once per frame
-	void Update () 
-	{
-		Steer(move.target.transform.position);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        Steer(move.target.transform.position);
+    }
 
-	public void Steer(Vector3 target)
-	{
-		if(!move)
-			move = GetComponent<Move>();
-
-        // Velocity we are trying to match
-        // TODO 3: Find the acceleration to achieve the desired velocity
-        // If we are close enough to the target just stop moving and do nothing else
-        // Calculate the desired acceleration using the velocity we want to achieve and the one we already have
-        // Use time_to_target as the time to transition from the current velocity to the desired velocity
-        // Clamp the desired acceleration and call move.AccelerateMovement()
+    public void Steer(Vector3 target)
+    {
+        if (!move)
+            move = GetComponent<Move>();
 
         Vector3 diff = target - transform.position;
+        float diff_magnitude = diff.magnitude;
 
-        if (min_distance > diff.magnitude)
-            move.SetMovementVelocity(Vector3.zero,priority);
-        else
+        diff.Normalize();
+        diff *= move.max_mov_acceleration;
+
+        Vector3 desired_acc = diff;
+
+        if (diff_magnitude < slow_distance)
         {
-            Vector3 diff_norm = diff.normalized * move.max_mov_velocity;
+            Vector3 desired_vel = diff.normalized * diff_magnitude * time_to_accel;
 
-            if (slow_distance > diff.magnitude)
-                diff_norm = diff_norm * (diff.magnitude / slow_distance);
+            if (diff_magnitude < min_distance)
+                desired_vel = Vector3.zero;
 
-            Vector3 desired_acc = diff_norm - move.current_velocity;
-            desired_acc /= time_to_accel;
-            if (desired_acc.magnitude > move.max_mov_acceleration)
-                desired_acc = desired_acc.normalized * move.max_mov_acceleration;
-
-            move.AccelerateMovement(desired_acc,priority);
-
+            desired_acc = desired_vel - move.current_velocity;
         }
-	}
 
-	void OnDrawGizmosSelected() 
-	{
-		// Display the explosion radius when selected
-		Gizmos.color = Color.white;
-		Gizmos.DrawWireSphere(transform.position, min_distance);
+        move.AccelerateMovement(desired_acc, priority);
+    }
 
-		Gizmos.color = Color.green;
-		Gizmos.DrawWireSphere(transform.position, slow_distance);
-	}
+    void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, min_distance);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, slow_distance);
+    }
 }
--- TANK 4/Assets/Steering/SteeringWander.cs
+++ TANK 4/Assets/Steering/SteeringWander.cs
@@ -4,9 +4,13 @@ using System.Collections;
 public class SteeringWander : Steering
 {
     public Vector3 offset = Vector3.zero;
-    public float radius = 1.0f;
-    public float min_update = 0.5f;
-    public float max_update = 3.0f;
+    public float radius = 5.0f;
+
+    private float timer = 2.5f;
+
+    public float update_time = 0.0f;
+    public float update_time_min = 2.0f;
+    public float update_time_max = 5.0f;
 
     SteeringSeek seek;
     Vector3 random_point;
@@ -15,24 +19,36 @@ public class SteeringWander : Steering
     void Start()
     {
         seek = GetComponent<SteeringSeek>();
-        ChangeTarget();
+        NewDirection();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= update_time)
+        {
+            NewDirection();
+            timer = 0.0f;
+        }
     }
 
     // Update is called once per frame
-    void ChangeTarget()
+    void NewDirection()
     {
+        update_time = Random.Range(update_time_min, update_time_max);
+
         random_point = Random.insideUnitSphere;
         random_point *= radius;
         random_point += transform.position + offset;
         random_point.y = transform.position.y;
 
-        seek.Steer(random_point,priority);
-        Invoke("ChangeTarget", Random.Range(min_update, max_update));
+        seek.Steer(random_point, priority);
     }
 
     void OnDrawGizmosSelected()
     {
-        if (this.isActiveAndEnabled)
+        if (isActiveAndEnabled)
         {
             // Display the explosion radius when selected
             Gizmos.color = Color.yellow;
